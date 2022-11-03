e8d2d81468557da01cfcd512be4d2168d699e5fc
Maggie Lively <maggielively@Maggie-Livelys-Pro.local>
2022-11-03 11:04:58 -0400

    added the add functions and made an attempt on the update function

--- db/index.js
+++ db/index.js
@@ -105,16 +105,133 @@ function startQuestions() {
             startQuestions();
           });
         });
-  
-    
-
-
-
-     
-   
       
    }
     
     
+
+   function addRole() {
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the name of the role you would like to add?",
+        name: "role_name",
+      },
+      {
+        type: "input",
+        message: "What is the salary of the role?",
+        name: "salary",
+      },
+      {
+        type: "input",
+        message: "What is the name of the department for this role?",
+        name: "department_name",
+      },
     
+    
+    ])
+    .then((answer) => {
+        const sqlR = `INSERT INTO role (name) VALUES (?)`;
+        const paramsR = [answer.role_name, answer.salary, answer.department_name];
+
+        db.query(sqlR, paramsR, function (err, results) {
+            if (err){
+                throw err;
+            }console.table(results);
+            startQuestions();
+          });
+        });
+      
+   }
+
+
+   function addEmployee() {
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the employee's first name?",
+        name: "firstname",
+      },
+      {
+        type: "input",
+        message: "What is the employee's last name?",
+        name: "lastname",
+      },
+      {
+        type: "input",
+        message: "What is their role?",
+        name: "role",
+      },
+      {
+        type: "input",
+        message: "Who is their manager?",
+        name: "manager",
+      },
+    
+    
+    ])
+    .then((answer) => {
+        const sqlE = `INSERT INTO employee (name) VALUES (?)`;
+        const paramsE = [answer.firstname, answer.lastname, answer.role, answer.manager];
+
+        db.query(sqlE, paramsE, function (err, results) {
+            if (err){
+                throw err;
+            }console.table(results);
+            startQuestions();
+          });
+        });
+      
+   }
+
+
+
+
+
+
+
+
+
+
+    
+   function  updateEmployee() {
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Which employee would you like to update?",
+        name: "update",
+      },
+
+      {
+        type: "input",
+        message: "What is their new role?",
+        name: "role",
+      },
+    
+    ])
+    .then((answer) => {
+        const sql2 = `UPDATE employee SET WHERE VALUES (?)`;
+        const params2 = [answer.name];
+
+        db.query(sql2, params2, function (err, results) {
+            if (err){
+                throw err;
+            }console.table(results);
+            startQuestions();
+          });
+        });
+
+    //   const sql2 = `UPDATE *, `
+
+    // db.query('UPDATE *, first_name, last_name, role_id, manager_id SET employee WHERE', function (err, results) {
+    //     if (err){
+    //         throw err;
+    //     }console.table(results);
+    //     startQuestions();
+    //   });
+      
+   }
    
